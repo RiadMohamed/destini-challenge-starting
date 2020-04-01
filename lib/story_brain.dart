f78c6d3c70def1d22789df6774dbfa638c9b7925
Riad Mohamed <riaduk1@gmail.com>
2020-04-01 10:23:59 +0200

    Create a method called getStory that return the title of the first story.
    Create a new StoryBrain object.
    
    The title label now calls the getStory method to show the title of the story.

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,7 +1,5 @@
-//TODO: Step 6 - import the story.dart file into this file.
 import 'story.dart';
 
-//TODO: Step 5 - Create a new class called StoryBrain.
 class StoryBrain {
   List<Story> _storyData = [
     Story(
@@ -34,13 +32,13 @@ class StoryBrain {
         choice1: 'Restart',
         choice2: '')
   ];
-}
-//TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
-
+  String getStory() {
+    return _storyData[0].storyTitle;
+  }
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
 //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
@@ -58,3 +56,4 @@ class StoryBrain {
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+}
